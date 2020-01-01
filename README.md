5e98fe5ce93c10a060df9179876af069d97d7ace
Jamie <kookjc6@gmail.com>
2020-01-01 12:32:08 -0600

    chained promises instead of nesting

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# DeveloperGen
+# Developer Profile Generator
+
+This node-based application returns a pdf resume displyaing information gathered from an inputed Github username. 
+
+![image of generated resume pdf](Resume.png)
+
+## Installation
+
+Clone folder onto your own computer by forking the repository and using the "Clone or download" button on github. Make sure that a working Google maps API key is entered in the index.js for the variable:
+
+const googleAPI=;    
+
+Run the program by calling node index.js from the commandline open in the root folder.
+
+## Functionality 
+
+### User Input
+Users will be asked for the Github username and the color theme for the generated resume in the command line. 
+
+### Github Infomation
+This application makes an api call to Github to gather the following information from the given username: 
+Profile image, location, link to Github profile, link to blog, the user's bio, number of public repositories, number of followers, number of Github stars, and number of user's he/she is following. 
+
+### Google Maps Location
+The pdf also includes a link to a Google map of the user's location. 
+
+## Coding
+
+*   This project utilizes javascript and css.
+*   Bootstraps 4 and Fontawesome are utilized for the styling.
+*   Inquier is used to gather user input from the command line. 
+*   Axios is used to make API calls.
+*   Github's api is called to gather input from a given username. 
+*   Google's geocoding api deteremines the lattitude and longitude from the given       city which is then create Google's map url to link to the location. 
+*   File System and Electron-html-to are used to create the pdf from the generated      html. 
+
+## Meta 
+
+Jamie Kook - kookjc6@gmail.com
+
+
--- index.js
+++ index.js
@@ -21,6 +21,11 @@ let followers = null;
 let stars = null;
 let following=null; 
 let name= null;  
+let queryUrl= null; 
+let data=null; 
+let username= null; 
+let lat= null; 
+let lng= null; 
 
 inquirer
   .prompt([
@@ -35,60 +40,46 @@ inquirer
       choices: ["green", "red", "blue", "pink"], 
       name: "color"
     }
-  ]).then(function(response){
-    const {username}= response;
-    const data= response; 
-    const queryUrl = `https://api.github.com/users/${username}`;
-    axios
-      .get(queryUrl)
-      .then(function(res){
-         imageUrl= res.data.avatar_url; 
-         location= res.data.location; 
-         html_url = res.data.html_url; 
-         blog= res.data.blog.toLowerCase(); 
-         bio= res.data.bio; 
-         public_repos= res.data.public_repos; 
-         followers = res.data.followers; 
-         following= res.data.following;
-         name= res.data.name; 
-         console.log(location);
-         const geolocateurl= `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${googleAPI}`;
-         console.log(geolocateurl); 
+  ])
+  .then(function(response){
+    username= response.username;
+    data= response; 
+    queryUrl = `https://api.github.com/users/${username}`;
+    return axios.get(queryUrl);
+  })
+  .then(function(res){
+    imageUrl= res.data.avatar_url; 
+    location= res.data.location; 
+    html_url = res.data.html_url; 
+    blog= res.data.blog.toLowerCase(); 
+    bio= res.data.bio; 
+    public_repos= res.data.public_repos; 
+    followers = res.data.followers; 
+    following= res.data.following;
+    name= res.data.name; 
+    const geolocateurl= `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${googleAPI}`;
+    return axios.get(geolocateurl);
+  })
+  .then(function(response){
+    lat= response.data.results[0].geometry.location.lat;
+    lng= response.data.results[0].geometry.location.lng;
+    return axios.get(queryUrl+"/starred");
+  })
+  .then(function(res){
+    stars=res.data.length;
+    const locationUrl= `https://www.google.com/maps/@?api=1&map_action=map&center=${lat},${lng}&zoom=14`;
+    const htmlData= generateHTML.generateHTML(data,imageUrl,location, locationUrl, html_url, blog, name, bio, public_repos, followers, stars, following);
+    conversion({html: htmlData, delay: 1000}, function(err, result){
+      if (err){
+        return console.log(err); 
+      } 
+      console.log(result.numberOfPages); 
+      console.log("success"); 
+      // console.log(result.logs); 
+      result.stream.pipe(fs.createWriteStream(username+".pdf"));
+      conversion.kill(); 
+    });     
+  });
 
-         axios
-          .get(geolocateurl)
-          .then(function(response){
-            console.log(response.data.results[0].geometry); 
-            const lat= response.data.results[0].geometry.location.lat;
-            const lng= response.data.results[0].geometry.location.lng;
-            console.log(lat, lng); 
-
-            axios
-            .get(queryUrl+"/starred")
-            .then(function(res){
-              stars=res.data.length;
-              const locationUrl= `https://www.google.com/maps/@?api=1&map_action=map&center=${lat},${lng}&zoom=14`;
-              const htmlData= generateHTML.generateHTML(data,imageUrl,location, locationUrl, html_url, blog, name, bio, public_repos, followers, stars, following);
-              conversion({html: htmlData, delay: 1000}, function(err, result){
-                  if (err){
-                    return console.log(err); 
-                  } 
-                  console.log(result.numberOfPages); 
-                  // console.log(result.logs); 
-                  result.stream.pipe(fs.createWriteStream(username+".pdf"));
-                  conversion.kill(); 
-                  });
-                fs.writeFile(username+".html", htmlData, function(err){
-                  if (err){
-                    console.log(err);
-                  } else{
-                    console.log("success"); 
-                    const htmlFile= fs.readFileSync(username+".html", "utf8");
-                  }
-                });       
-            }); 
-          });
-      });
-  }) 
 
   
